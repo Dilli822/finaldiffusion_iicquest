1b1ac2887f1aef9c3155b64ec759d2864ffd262e
Dilli Hang Rai <76522863+Dilli822@users.noreply.github.com>
2025-06-12 08:58:11 +0545

    Delete backend directory

--- backend/routes/notification.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { sendNotification } from "../controllers/notification.controller.js";
-
-const router = express.Router();
-
-router.post("/send", sendNotification);
-
-export default router;
