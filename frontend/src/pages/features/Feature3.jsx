5b03c94f2c9e60b1ac2bfeb036de89d235526b41
Dilli Hang Rai <76522863+Dilli822@users.noreply.github.com>
2025-06-12 08:58:25 +0545

    Delete frontend directory

--- frontend/src/pages/features/Feature3.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Feature3() {
-  return (
-    <div>Feature3</div>
-  )
-}
-
-export default Feature3
--- frontend/src/pages/features/Feature4.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Feature4() {
-  return (
-    <div>Feature4</div>
-  )
-}
-
-export default Feature4
